402c3877b9f6e43b51fab1ef626136556f249bec
Padmanabhan <padmanabhansridhar2005@gmail.com>
2026-02-15 03:34:41 +0530

    feat: implement context retrieval from Pinecone for RAG

---
+++ src/lib/context.ts
@@ -0,0 +1,44 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { getPineconeClient } from "./pinecone";
+import { convertToASCII } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(embeddings: number[], file_key: string) {
+    const pinecone = await getPineconeClient();
+    const index = pinecone.index(process.env.PINECONE_INDEX_NAME || "chatpdf");
+
+    try {
+        const namespace = convertToASCII(file_key);
+        const pineconeNamespace = index.namespace(namespace);
+        const queryResult = await index.query({
+            topK: 5,
+            vector: embeddings,
+            includeMetadata: true,
+            namespace
+        })
+        return queryResult.matches || [];
+    } catch (error) {
+        console.error("Error getting matches from embeddings", error);
+        throw error;
+    }
+
+}
+
+
+export async function getContext(query: string, file_key: string) {
+    const queryEmbeddings = await getEmbeddings(query);
+    const matches = await getMatchesFromEmbeddings(queryEmbeddings, file_key);
+
+    const qualifyingDocs = matches.filter((match) => match.score && match.score > 0.7);
+
+    type Metadata = {
+        text: string;
+        pageNumber: number;
+    }
+
+    let docs = qualifyingDocs.map(match => (match.metadata as Metadata).text);
+
+    return docs.join("\n").substring(0, 3000);
+
+}
+
